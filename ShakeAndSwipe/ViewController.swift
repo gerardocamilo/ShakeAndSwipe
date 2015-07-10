02217f04b14636e10e56e1ecae1c01dd5641f49a
Gerardo Camilo <gerardo.camilo@gmail.com>
2015-07-09 22:43:50 -0400

    Adding swipe recognisers to the app. The app now identifies the different swipes: right, left, up and down.

--- ShakeAndSwipe/ViewController.swift
+++ ShakeAndSwipe/ViewController.swift
@@ -12,9 +12,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        var swipeLeft = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        var swipeUp = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        var swipeDown = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.view.addGestureRecognizer(swipeDown)
+        
     }
 
+    func swiped(gestureRecognizer: UIGestureRecognizer){
+        if let gesture = gestureRecognizer as? UISwipeGestureRecognizer {
+            
+            switch(gesture.direction){
+            case UISwipeGestureRecognizerDirection.Right:
+                    println("Swiped Right!")
+            case UISwipeGestureRecognizerDirection.Left:
+                println("Swiped Left!")
+            case UISwipeGestureRecognizerDirection.Up:
+                println("Swiped Up!")
+            case UISwipeGestureRecognizerDirection.Down:
+                println("Swiped down!")
+                
+                
+            default:
+                break
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
